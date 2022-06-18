922be4c69287d24a665c85f53b291488af05ac10
Robert Routhier <robrouth92@gmail.com>
2022-06-18 14:42:07 -0700

    seeded

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,13 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  : new Sequelize(process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, 
+    {
       host: 'localhost',
       dialect: 'mysql',
-      port: process.env || 3306,
+      port: 8889,
       dialectOptions: {
       decimalNumbers: true,
     },
