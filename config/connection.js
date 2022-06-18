b762a76c7d94fb0c4181f1acdff664bf0b1dd1da
Robert Routhier <robrouth92@gmail.com>
2022-06-18 11:59:18 -0700

    cannot seed

--- config/connection.js
+++ config/connection.js
@@ -7,10 +7,10 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
+      port: process.env || 3306,
       dialectOptions: {
       decimalNumbers: true,
     },
-    port: 8889,
     });
 
 module.exports = sequelize;
